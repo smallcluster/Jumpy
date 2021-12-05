3f35768f2b3edfc1ca56ee99d673d43a71a463ca
smallcluster <small.cluster@gmail.com>
2021-12-05 19:53:23 +0400

    game activity

--- app/src/main/java/com/smallcluster/jumpy/GameActivity.java
+++ app/src/main/java/com/smallcluster/jumpy/GameActivity.java
@@ -1,6 +1,15 @@
 package com.smallcluster.jumpy;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
 }
