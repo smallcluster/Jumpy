2bd9bb868c8a5059fd3843ceaa2a4531b69207ac
smallcluster <small.cluster@gmail.com>
2021-12-06 12:59:23 +0400

    Décor parallax

--- app/src/main/java/com/smallcluster/jumpy/GameView.java
+++ app/src/main/java/com/smallcluster/jumpy/GameView.java
@@ -19,6 +19,8 @@ l'affichage est redimensionné en conservant le ratio.
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
 
+    private Paint paint = new Paint();
+
     // contient les bordures de la surface
     private SurfaceHolder holder = null;
     // thread du jeu
@@ -28,18 +30,26 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
     // test du thread
     private boolean updating = false;
 
+
     // 60 fps max
-    private static final int MAX_FRAME_TIME = (int) (1000.0 / 60.0);
-    private float delta = MAX_FRAME_TIME / 1000.0f;
+    private static final int MAX_FRAME_TIME = 16;
+    private float delta = 0.016f;
 
 
     // --------------- IMAGES ----------------------
-    private Bitmap BG_BMP = BitmapFactory.decodeResource(getResources(), R.drawable.bg);
+    private Bitmap CIEL_BMP = BitmapFactory.decodeResource(getResources(), R.drawable.ciel);
+    private Bitmap COLLINES_BMP = BitmapFactory.decodeResource(getResources(), R.drawable.collines);
+    private Bitmap NUAGE_BMP = BitmapFactory.decodeResource(getResources(), R.drawable.nuage);
+    private Bitmap SOL_BMP = BitmapFactory.decodeResource(getResources(), R.drawable.sol);
 
     // ------------------------ MONDE --------------
 
+
+
     private float vitesse = 250; // en pixels/s
-    private float decalageBG = 0;
+    private float decalageSol = 0;
+    private float decalageCollines = 0;
+    private float decalageNuage = 0;
 
 
     public void init(){
@@ -49,22 +59,42 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
     }
 
     public void render(Canvas c){
-        Paint paint = new Paint();
 
+        // ciel fixe
+        c.drawBitmap(CIEL_BMP, 0, 0, null);
 
-        c.drawBitmap(BG_BMP, -decalageBG, 0, paint);
-        c.drawBitmap(BG_BMP, -decalageBG+BG_BMP.getWidth(), 0, paint);
+        // collines
+        c.drawBitmap(COLLINES_BMP, 38-decalageCollines, 423, null);
+        c.drawBitmap(COLLINES_BMP, 38-decalageCollines+1280, 423, null);
 
-        decalageBG += vitesse*delta;
-        if(decalageBG > c.getWidth())
-            decalageBG = 0;
+        // sol
+        c.drawBitmap(SOL_BMP, -decalageSol, 581, null);
+        c.drawBitmap(SOL_BMP, -decalageSol+1280, 581, null);
 
+        // nuage
+        c.drawBitmap(NUAGE_BMP, 18-decalageNuage, 34, null);
+        c.drawBitmap(NUAGE_BMP, 18-decalageNuage+1280, 34, null);
 
         paint.setColor(Color.RED);
         c.drawText("fps: "+(int) (1.0f/delta), 32, 32, paint);
     }
 
     public void update(){
+        // --------- PARALLAX -------------
+        // collines
+        decalageCollines += vitesse*0.25f*delta;
+        if(decalageCollines >= 1280)
+            decalageCollines = 0;
+
+        // sol
+        decalageSol += vitesse*delta;
+        if(decalageSol >= 1280)
+            decalageSol = 0;
+
+        // nuage
+        decalageNuage += vitesse*1.5f*delta;
+        if(decalageNuage >= 1280)
+            decalageNuage = 0;
 
     }
 
@@ -74,15 +104,14 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
         if (width == 0 || height == 0){
             return;
         }
-
         // ajuster les tailles
-
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder){
         this.holder = holder;
         holder.setFixedSize(1280, 720); // buffer interne en 720p
+        holder.setKeepScreenOn(true);
         if (updateThread != null){
             updating = false;
             try{
@@ -156,7 +185,9 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
             }
             frameStartTimeNano = System.nanoTime();
             Canvas canvas = null;
-            canvas = holder.lockHardwareCanvas(); // canvas GPU
+            if(holder.getSurface().isValid()) {
+                canvas = holder.lockHardwareCanvas();
+            }
             if (canvas != null) {
                 try {
                     synchronized (holder) {
@@ -170,12 +201,15 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
 
             // Calcul du temps d'attente pour avoir 60 fps au maximum
             frameTime = (System.nanoTime() - frameStartTimeNano) / 1000000;
+
             if (frameTime < MAX_FRAME_TIME){
                 try {
                     Thread.sleep(MAX_FRAME_TIME - frameTime);
+                    delta = 0.016f;
                 } catch (InterruptedException e) { }
+            } else {
+                delta = frameTime / 1000.0f;
             }
-            delta = (System.nanoTime() - frameStartTimeNano) / 1000000000.0f;
         }
     }
 }
